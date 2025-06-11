15c9f27e5f96147738c9a6700a2751c8782a163d
RetroRoom17 <163794395+RetroRoom17@users.noreply.github.com>
2025-06-10 19:48:27 -0600

    Updated games list

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const games = [
   { title: "Minecraft", background: "wallpapers/minecraft.jpg", slogan: "Build, explore, survive—your adventure awaits!" },
   { title: "Halo 3", background: "wallpapers/halo3.jpg", slogan: "Finish the Fight" },
-  { title: "DOOM", background: "wallpapers/DOOM.jpg", slogan: "Rip & Tear, until it is done." },
+  { title: "DOOM", background: "wallpapers/DOOM.jpg", slogan: "Fight Like Hell" },
   { title: "Skyrim", background: "wallpapers/skyrim.jpg", slogan: "You are... Dragonborn!" },
   { title: "The Elder Scrolls IV", background: "wallpapers/elderscrollsoblivionremaster.jpg", slogan: "Close shut the Jaws... of Oblivion." },
   { title: "Tron Evolution", background: "wallpapers/tronevolution.jpg", slogan: "Welcome to The Grid, Program." },
   { title: "Devil May Cry 5", background: "wallpapers/dmc5.jpg", slogan: "I Am The Storm That Is Approaching" },
-  { title: "DOOM 2016", background: "wallpapers/DOOM2016.jpg", slogan: "Fight Like Hell" },
+  { title: "DOOM 2016", background: "wallpapers/DOOM2016.jpg", slogan: "Rip and Tear, until it is Done." },
+  { title: "Portal 2", background: "wallpapers/portal2.jpg", slogan: "The Return of GlaDOS" },
+  { title: "Half-Life", background: "wallpapers/halflife.jpg", slogan: "The Right Man in the Wrong Place Can Make All The Difference In The World" },
+  { title: "Halo: Reach", background: "wallpapers/haloreach.jpg", slogan: "From the Beginning, You Know the End." },
+  { title: "Star Wars: The Force Unleashed", background: "wallpapers/starwarstheforceunleashed.jpg", slogan: "He was meant to unite us. To help us rise from the shadows of the Empire." },
+  { title: "Star Wars: Empire At War", background: "wallpapers/starwarsempireatwar.jpg", slogan: "The rebellion must succeed… or all hope is lost." },
+  { title: "Destiny", background: "wallpapers/destiny.jpg", slogan: "Eyes Up, Guardian!" },
+  { title: "Destiny 2", background: "wallpapers/destiny2.jpg", slogan: "Welcome to a World... Without Light." },
+  { title: "Forza Horizon", background: "wallpapers/forzahorizon.jpg", slogan: "Leave Your Limits" },
+  { title: "Cyberpunk 2077", background: "wallpapers/cyberpunk2077.jpg", slogan: "Wake Up Samurai, we have a city to burn." },
   { title: "Halo: Combat Evolved", background: "wallpapers/haloce.jpg", slogan: "Combat has Evolved." }
 ];
 
